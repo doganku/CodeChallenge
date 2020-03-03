cc3563df9ea5e7afee40aa31afc2ba1cd378afd1
Kuntay Dogan <kdo@hellofresh.com>
2020-03-03 19:11:18 +0100

    deleted report files

--- output/1583250083807_test.js
+++
@@ -1,8 +0,0 @@
-import testControllerHolder from "/Users/kuntay.dogan/Documents/Projects/CodeChallenge/node_modules/codeceptjs/lib/helper/testcafe/testControllerHolder.js";
-
-
-    fixture("fixture")
-
-    test
-
-    ("test", testControllerHolder.capture)
